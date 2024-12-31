a5800f78f700fdf143e9db0b95672b479053cb28
zhd-love <72731964+zhd-love@users.noreply.github.com>
2024-12-31 10:33:03 +0800

    Update build.sh

--- x86-64/build.sh
+++ x86-64/build.sh
@@ -22,6 +22,9 @@ PACKAGES="$PACKAGES luci-app-openclash"
 PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
 PACKAGES="$PACKAGES openssh-sftp-server"
 PACKAGES="$PACKAGES luci-i18n-dockerman-zh-cn"
+PACKAGES="$PACKAGES luci-app-adguardhome"
+PACKAGES="$PACKAGES luci-app-unblockmusic"
+PACKAGES="$PACKAGES luci-app-vlmcsd"
 
 # 构建镜像
 echo "$(date '+%Y-%m-%d %H:%M:%S') - Building image with the following packages:"
